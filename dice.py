0a7e99fb3765927947eb0620f8cd37055171b349
hezykiahfreeze <70069347+hezykiahfreeze@users.noreply.github.com>
2025-01-03 07:48:53 +0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,68 @@
+import random
+h = random.randint(1,6)
+# h=1
+if h == 1:
+    print(f"""
+         
+      #  
+        """)
+if h == 2:
+    print(f"""
+    #    
+
+        # """)
+if h == 3:
+    print(f"""
+    #    
+      #  
+        # """)
+if h == 4:
+    print(f"""
+    #   #
+         
+    #   # """)
+if h == 5:
+    print(f"""
+    #   #
+      #  
+    #   # """)
+if h == 6:
+    print(f"""
+    #   #
+    #   #
+    #   # """)
+    
+h = random.randint(1,6)
+# h=1
+if h == 1:
+    print(f"""
+                   
+      #  
+         """)
+if h == 2:
+    print(f"""
+    #    
+
+        # """)
+if h == 3:
+    print(f"""
+    #    
+      #  
+        # """)
+if h == 4:
+    print(f"""
+    #   #
+         
+    #   # """)
+if h == 5:
+    print(f"""
+    #   #
+      #  
+    #   # """)
+if h == 6:
+    print(f"""
+    #   #
+    #   #
+    #   # """)    
+# print(h)
+
